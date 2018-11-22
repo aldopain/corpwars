6dec3d25dfab27114b9ffec8a4e9f475abd7d4d2
fshpsmgc <fshpsmgc@gmail.com>
2018-11-22 18:16:37 +0300

    Added basic units, fixed #2

---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour {
+	public NavigationNode[] Nodes;
+	
+	private NavMeshAgent agent;
+	private int patrolDest;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(Nodes[0].transform.position);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(agent.remainingDistance < 0.05f){
+			agent.SetDestination(Nodes[patrolDest].transform.position);
+			patrolDest = (patrolDest + 1) % Nodes.Length;
+		}
+	}
+}
---
+++ Assets/Scripts/Units/UnitStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour {
+	public float Speed;
+	public int Attack;
+	public int Defence;
+	public int CargoCapacity;
+}
