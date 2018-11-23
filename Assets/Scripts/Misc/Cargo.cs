35a1f68a1fc7d85a0112236cfb9a2a2e43ac03ec
fshpsmgc <fshpsmgc@gmail.com>
2018-11-23 22:00:18 +0300

    Fixed #3

---
+++ Assets/Scripts/Misc/Cargo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Cargo{
+	public enum CargoType{
+		Type1, Type2, Type3
+	}
+
+	public CargoType CurrentType;
+	public float MaxAmount;
+	private float Amount;
+
+	public Cargo(CargoType type, float max = Mathf.Infinity, float amount = 0){
+		CurrentType = type;
+		MaxAmount = max;
+
+		if(Amount > MaxAmount){
+			Amount = MaxAmount;
+		}
+		Amount = amount;
+	}
+
+	public void Add(float a){
+		if(Amount + a > MaxAmount){
+			Amount = MaxAmount;
+		}else if(Amount + a < 0){
+			Amount = 0;
+		}else{
+			Amount += a;
+		}
+	}
+
+	/* Returns:
+			if more than max after addition: positive number representing the storage needed to store surplus
+			if less than zero after addition: negative number representing amount of cargo needed to detract a from pile
+			if can add to the pile: 0
+	 */
+	public float GetDifference(int a){
+		if(Amount + a > MaxAmount){
+			return Amount + a - MaxAmount;
+		}else if(Amount + a < 0){
+			return Amount + a;
+		}else{
+			return 0;
+		}
+	}
+
+	public float GetAmount(){
+		return Amount;
+	}
+}
---
+++ Assets/Scripts/Units/UnitInventory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitInventory : MonoBehaviour {
+	public List<Cargo> Inventory;
+
+	public int MaxNumber;
+	public float MaxAmount;
+
+	public bool AddCargo(Cargo.CargoType type, float Amount){
+		if(Inventory.Count < MaxNumber){
+			if(Amount > MaxAmount){
+				return false;
+			}
+			Inventory.Add(new Cargo(type, MaxAmount, Amount));
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public bool AddCargo(Cargo c){
+		if(Inventory.Count < MaxNumber){
+			if(c.GetAmount() < MaxAmount){
+				Inventory.Add(c);
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+
+	public bool RemoveCargo(Cargo c){
+		if(Inventory.Contains(c)){
+			Inventory.Remove(c);
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
