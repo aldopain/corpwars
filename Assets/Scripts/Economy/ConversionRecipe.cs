9271c1f8a73ad6d01a4a70ef355c1cac8e989a5e
fshpsmgc <fshpsmgc@gmail.com>
2018-11-28 19:15:17 +0300

    fixed #10

---
+++ Assets/Scripts/Economy/ConversionRecipe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ConversionRecipe{
+	public TradeResource.Types[] input;
+	public float[] inputAmount;
+
+	public TradeResource.Types output;
+	public float outputAmount;
+}
--- Assets/Scripts/Economy/LocalEconomy.cs
+++ Assets/Scripts/Economy/LocalEconomy.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LocalEconomy : MonoBehaviour {
 	public List<TradeResource> resources;
+	public List<ConversionRecipe> recipes;
 
 	// Use this for initialization
 	void Start () {
 		SetResorcesList();
+		GameObject.FindGameObjectWithTag("GameController").GetComponent<Time>().OnDay.AddListener(ConvertResources);
 	}
 	
 	// Update is called once per frame
@@ -36,5 +38,15 @@ public class LocalEconomy : MonoBehaviour {
 
 	public void Remove(Cargo c){
 		FindResource(c.CurrentType).stock -= c.GetAmount(); 
+	}
+
+	public void ConvertResources(){
+		for(int i = 0; i < recipes.Count; i++){
+			for(int j = 0; j < recipes[i].input.Length; i++){
+				FindResource(recipes[i].input[j]).stock -= recipes[i].inputAmount[j];
+			}
+
+			FindResource(recipes[i].output).stock += recipes[i].outputAmount;
+		}
 	}
 }
