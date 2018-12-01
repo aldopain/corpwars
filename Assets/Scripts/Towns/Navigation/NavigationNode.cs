165be2cf074aefd7e412ca3ddd137fdd691ee3c4
Jaan Toming <j.toming@gaijin.lan>
2018-12-01 14:42:47 +0300

    navsystem WIP

---
+++ Assets/Scripts/Towns/Navigation/NavigationNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationNode : MonoBehaviour {
+	public List<NavigationNode> Neighbours;
+	List<NavigationWay> ways;
+	public Collider[] nearby;
+
+	void Update(){
+		nearby = Physics.OverlapSphere(transform.position, 10);
+	}
+
+	public float Distance (NavigationNode p) {
+		return Vector3.Distance(transform.position, p.transform.position);
+	}
+
+}
---
+++ Assets/Scripts/Towns/Navigation/NavigationSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationSystem : MonoBehaviour {
+
+	List<NavigationNode> allPoints;
+	List<NavigationWay> ways;
+
+	// Use this for initialization
+	void Start () {
+		allPoints = new List<NavigationNode>();
+		var tmp = GameObject.FindGameObjectsWithTag("TradePost");
+		foreach (var current in tmp)
+			allPoints.Add(current.GetComponent<NavigationNode>());
+	}
+
+	void Dijkstra (NavigationNode s) {
+		var tmpWays = new List<NavigationWay>();
+		foreach ( var current in allPoints) {
+			if (!current.Equals(s))
+				if (s.Neighbours.Contains(current)) {
+					var tmp = new List<NavigationNode>();
+				} else
+					tmpWays.Add (new NavigationWay(s, current));
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
