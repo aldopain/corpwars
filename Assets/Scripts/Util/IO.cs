4b627c393191c658630382152d63715ed3dfcf8c
Jaan Toming <j.toming@gaijin.lan>
2019-03-04 15:39:32 +0300

    added IO script to nice formated output and (maybe) input

---
+++ Assets/Scripts/Util/IO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IO {
+	public static void Out<T>(List<T> p){
+		Debug.Log(string.Join("", p.ConvertAll(i => i.ToString()).ToArray()));
+	}
+}
