b168d7429cdf4dc9f111002f5f5c27e2574460e0
Jaan Toming <j.toming@gaijin.lan>
2019-03-05 16:05:20 +0300

    little refactoring and resolved #45

--- Assets/Scripts/Towns/Town_Population.cs
+++ Assets/Scripts/Towns/Town_Population.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// this class also should consume resources
+// _as I think_
 public class Town_Population : MonoBehaviour {
 	[System.Serializable]
 	public struct PopClass{
@@ -13,8 +15,9 @@ public class Town_Population : MonoBehaviour {
 		public int Happiness;
 		public double[] Demands;
 
-		public void Update(){
-			UpdateHappiness();
+		// resources - Actor_Resources.Amount
+		public void Update(double[] resources){
+			UpdateHappiness(resources);
 			UpdateProductivity();
 			UpdateDemands();
 		}
@@ -24,13 +27,22 @@ public class Town_Population : MonoBehaviour {
 		}
 
 		//Function that would calculate happiness based on available resources
-		void UpdateHappiness(){
-			//Placeholder
+		void UpdateHappiness(double[] resources){
+			Happiness = (int) (DemandLevel(resources) * 100);
+		}
+
+		double DemandLevel (double[] resources){
+			double d = 0;
+			if (Demands.Length != resources.Length) Debug.LogWarning("Demands.Length != resources.Length");
+			for(int i = 0; i < resources.Length && i < Demands.Length; i++){
+				d += (resources[i] > Demands[i] ? 1 : resources[i] / Demands[i]);
+			}
+			return d / Demands.Length;
 		}
 
 		//Function, that would calculate demands
 		void UpdateDemands(){
-
+			
 		}
 	}
 
@@ -41,18 +53,19 @@ public class Town_Population : MonoBehaviour {
 
 	void Start(){
 		Resource_GlobalList gl = GameObject.Find("GameController").GetComponent<Resource_GlobalList>();
-		
-		Poor.Demands = new double[gl.ResourcesList.Length];
+		var glResLength = gl.ResourcesList.Length;
+
+		Poor.Demands = new double[glResLength];
 		for(int i = 0; i < Poor.Demands.Length;i++){
 			Poor.Demands[i] = Random.Range(0, 100);
 		}
 
-		Middle.Demands = new double[gl.ResourcesList.Length];
+		Middle.Demands = new double[glResLength];
 		for(int i = 0; i < Middle.Demands.Length;i++){
 			Middle.Demands[i] = Random.Range(0, 100);
 		}
 
-		Middle.Demands = new double[gl.ResourcesList.Length];
+		Middle.Demands = new double[glResLength];
 		for(int i = 0; i < Middle.Demands.Length;i++){
 			Middle.Demands[i] = Random.Range(0, 100);
 		}
--- Assets/Scripts/Towns/Town_Production.cs
+++ Assets/Scripts/Towns/Town_Production.cs
@@ -25,18 +25,19 @@ public class Town_Production : MonoBehaviour {
 
 	void Produce(int index){
 		Resource_GlobalList gl = GameObject.Find("GameController").GetComponent<Resource_GlobalList>();					//get list of all resources
-		
-		foreach(Resource_Input input in gl.ResourcesList[index].Recipe.input){											//check if town has all components
+		Resource_ConvertionRecipe recipe = gl.ResourcesList[index].Recipe;
+	
+		foreach(Resource_Input input in recipe.input){											//check if town has all components
 			if(!economy.resources.CheckResource(input.inputID, input.amount * inputModifier[input.inputID])) return;
 		}
 
-		for(int i = 0; i < gl.ResourcesList[index].Recipe.input.Length; i++){											//detract base resources that are needed to create new one 
-			economy.resources.RemoveResource(gl.ResourcesList[index].Recipe.input[i] * inputModifier[gl.ResourcesList[index].Recipe.input[i].inputID]);
+		for(int i = 0; i < recipe.input.Length; i++){											//detract base resources that are needed to create new one 
+			economy.resources.RemoveResource(recipe.input[i] * inputModifier[recipe.input[i].inputID]);
 		}
 
-        for(int i = 0; i < gl.ResourcesList[index].Recipe.output.Length; i++)                                           //add new resource to the pile
+        for(int i = 0; i < recipe.output.Length; i++)                                           //add new resource to the pile
         {
-            economy.resources.AddResource(gl.ResourcesList[index].Recipe.output[i] * outputModifier[gl.ResourcesList[index].Recipe.output[i].inputID]);
+            economy.resources.AddResource(recipe.output[i] * outputModifier[recipe.output[i].inputID]);
         }
 		                    																		
 
