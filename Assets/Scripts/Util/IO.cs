841c35170eae11cb93358af2e63d6c3c875bf6a7
Jaan Toming <j.toming@gaijin.lan>
2019-03-07 19:09:55 +0300

    added more functionality to IO util

--- Assets/Scripts/Util/IO.cs
+++ Assets/Scripts/Util/IO.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class IO {
 	public static void Out<T>(List<T> p){
-		Debug.Log(string.Join("", p.ConvertAll(i => i.ToString()).ToArray()));
+		Out(p, " ");
+	}
+
+	public static void Out<T>(List<T> p, string separator){
+		Debug.Log(ListToString(p, separator));
+	}
+
+	public static string ListToString<T>(List<T> p){
+		return ListToString(p, " ");
+	}
+
+	public static string ListToString<T>(List<T> p, string separator){
+		var s = string.Join(separator, p.ConvertAll(i => i.ToString()).ToArray());
+		return s;
 	}
 }
