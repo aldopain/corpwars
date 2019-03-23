95d89042dbb6d703b9ed3c9f1d547b38f2ef2d6e
fshpsmgc <fshpsmgc@gmail.com>
2019-03-23 17:08:56 +0300

    Started xUI; Created draggable windows and managers

---
+++ Assets/Scripts/xUI/xUI_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xUI_Manager : MonoBehaviour {
+    xUI_WindowManager windows;
+    xUI_TopPanel topPanel;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Confined;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/xUI/xUI_Window.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class xUI_Window : MonoBehaviour {
+    public Button CloseButton;
+    public RectTransform TopPanel;
+    public RectTransform ContentPanel;
+    //[HideInInspector]
+    public xUI_WindowManager manager;
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.Find("GameController").GetComponent<xUI_WindowManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowOnTop()
+    {
+        manager.PutOnTop(this);
+    }
+
+    public void DragWindow()
+    {
+        ShowOnTop();
+        Vector2 pos = Input.mousePosition;
+        pos.y += transform.position.y - TopPanel.position.y;
+        transform.position = pos;
+    }
+
+    public void Close()
+    {
+        Destroy(gameObject);
+    }
+}
