b83f9697e9680148b9a5d5a6e93f50e2fa5b5ed2
fshpsmgc <fshpsmgc@gmail.com>
2019-03-24 21:06:26 +0300

    Started xUI tooltip system

---
+++ Assets/Scripts/xUI/xUI_Tooltip.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class xUI_Tooltip : MonoBehaviour {
+    public RectTransform Panel;
+    public Text TooltipGO;
+
+	// Use this for initialization
+	void Start () {
+        print(Panel.sizeDelta);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Panel.sizeDelta);
+        MoveToMousePosition();
+	}
+
+    public void Show(string tooltip)
+    {
+        ResizeToText(tooltip);
+        TooltipGO.text = tooltip;
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void MoveToMousePosition()
+    {
+        Vector2 pos = Input.mousePosition;
+        pos.x += TooltipGO.rectTransform.sizeDelta.x/2;
+        pos.y -= TooltipGO.rectTransform.sizeDelta.y;
+        transform.position = pos;
+    }
+
+    void ResizeToText(string text)
+    {
+        Panel.sizeDelta = FindBackgroundDimensions(text) * TooltipGO.fontSize;
+        TooltipGO.rectTransform.sizeDelta = FindBackgroundDimensions(text) * TooltipGO.fontSize;
+    }
+
+    Vector2 FindBackgroundDimensions(string text)
+    {
+        return new Vector2(text.Length, 3);
+    }
+}
