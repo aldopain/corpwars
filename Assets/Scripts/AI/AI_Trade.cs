ebf7ed5cdb9e7490954d7b5e526300205beae57a
Jaan Toming <jtoming@gmail.com>
2019-03-27 18:26:22 +0300

    init commit

---
+++ Assets/Scripts/AI/AI_Trade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Trade : MonoBehaviour {
+	public void ChooseRoutes () {
+
+	}
+
+	private void GenerateRoutes () {
+		
+	}
+}
