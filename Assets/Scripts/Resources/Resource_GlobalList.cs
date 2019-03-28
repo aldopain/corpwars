5348c5168c5699e0075f98cbf4d7d8397c28786f
Jaan Toming <jtoming@gmail.com>
2019-03-28 16:48:43 +0300

    deleted editor_resource and fixed global list

--- Assets/Scripts/Resources/Resource_GlobalList.cs
+++ Assets/Scripts/Resources/Resource_GlobalList.cs
@@ -3,9 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Resource_GlobalList : MonoBehaviour {
-	Resource[] ResourcesList;
-
-	public Resource GetById(int id){
-		return ResourcesList[id];
-	}
+	public Resource[] ResourcesList;
 }
