1edb372e9e002047d13a3e3bf390a2f21da43ffd
Apple Dash <fshpsmgc@gmail.com>
2019-04-02 13:02:32 +0300

    Added tabbed windows

---
+++ Assets/Scripts/xUI/xUI_Tab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class xUI_Tab : MonoBehaviour {
+    public Button LabelButton;
+    public RectTransform Content;
+
+    public void Enable()
+    {
+        LabelButton.interactable = false;
+        Content.gameObject.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        LabelButton.interactable = true;
+        Content.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/xUI/xUI_WindowTabs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xUI_WindowTabs : MonoBehaviour {
+    public List<xUI_Tab> Tabs;
+    public xUI_Tab selectedTab;
+
+    void Start()
+    {
+        foreach(xUI_Tab tab in Tabs)
+        {
+            tab.Disable();
+        }
+        selectedTab = Tabs[0];
+        SelectTab(0);
+    }
+
+    public void SelectTab(int index)
+    {
+        SelectTab(Tabs[index]);
+    }
+
+    public void SelectTab(xUI_Tab tab)
+    {
+        selectedTab.Disable();
+        tab.Enable();
+        selectedTab = tab;
+    }
+}
