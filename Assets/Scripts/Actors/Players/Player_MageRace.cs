e6dd9f292f26d7ddf8fba9ab45c5c3ee23fefd2a
Jaan Toming <jtoming@gmail.com>
2019-04-17 16:11:56 +0300

    added mage race

---
+++ Assets/Scripts/Actors/Players/Player_MageRace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_MageRace : Player_Trade {
+	System_Time time; 
+	void Start() {
+		resources = GetComponent<Actor_Resources>();
+		money = GetComponent<Player_Money>();
+		events = gm.GetComponent<System_EventPool>();
+		time = gm.GetComponent<System_Time>();
+		time.OnMonth.AddListener(RefreshResources);
+	}
+
+	void RefreshResources(){
+		resources.Randomize();
+	}
+}
