dad2534e76e7566672234d954f7af29f1469fc84
LazyLahtak <lazylahtak@yandex.ru>
2019-06-24 00:20:11 +0300

    Add Dijkstra method to NavigationSystem

--- Assets/Scripts/Navigation/NavigationSystem.cs
+++ Assets/Scripts/Navigation/NavigationSystem.cs
@@ -37,7 +37,62 @@ public class NavigationSystem : MonoBehaviour {
 	}
 
 	private int[] Dijkstra(float[,] matrix, int a, int b) {
+		float[] dist = new float[ways.GetLength(0)];
+		int[] prev = new int[ways.GetLength(0)];
+		bool[] visited = new bool[ways.GetLength(0)];
+		List<int> result = new List<int>();
 
+		for (int i = 0; i < dist.Length; i++)
+		{
+			dist[i] = Single.PositiveInfinity;
+			prev[i] = -1;
+			visited[i] = false;
+		}
+		dist[start] = 0;
+
+		while (true)
+		{
+			float max = Single.PositiveInfinity;
+			int current = -1;
+
+			for (int i = 0; i < visited.Length; i++)    //Ищем непосещенную вершину с кратчайшим путем
+			{
+				if (!visited[i] && dist[i] < max) 
+				{
+					max = dist[i];
+					current = i;
+				}
+			}
+
+			if(current < 0)
+			{
+				break;
+			}
+
+			visited[current] = true;
+
+			for (int i = 0; i < dist.Length; i++)   //Обновляем все пути из неё
+			{
+				if (i != current)
+				{
+					float newDist = dist[current] + ways[current, i];
+					if (newDist < dist[i])
+					{
+						dist[i] = newDist;
+						prev[i] = current;
+					}
+				}
+
+			}
+		}
+		
+		for (int i = end; i >= 0; i = prev[i])
+		{
+			result.Add(i);
+		}
+		result.Reverse();
+
+		return result.ToArray();
 	}
 
 	private NavigationWay Convert(int[] way) {
