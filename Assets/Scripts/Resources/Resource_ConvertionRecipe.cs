2ccf5b5192e656818d04a183b5bb2c5785621d93
aldopain <jtoming@gmail.com>
2019-07-15 19:12:49 +0300

    clean up

--- Assets/Scripts/Resources/Resource_ConvertionRecipe.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class Resource_ConvertionRecipe {
-	public Resource_Input[] input;
-    public Resource_Input[] output;
-}
--- Assets/Scripts/Resources/Resource_GlobalList.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Resource_GlobalList : MonoBehaviour {
-	public Resource[] ResourcesList;
-}
